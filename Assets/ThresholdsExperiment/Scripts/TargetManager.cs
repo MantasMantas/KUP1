a8d0e850da52cc9a7341bae7a9223caedd308829
MantasMantas <xmantissx@gmail.com>
2023-09-29 16:57:54 +0200

    vibration measures started

--- Assets/ThresholdsExperiment/Scripts/TargetManager.cs
+++ Assets/ThresholdsExperiment/Scripts/TargetManager.cs
@@ -9,8 +9,20 @@ public class TargetManager : MonoBehaviour
     public TargetPositions targetPositions;
     // Start is called before the first frame update
 
+
+    private void Start()
+    {
+        TargetDemo();   
+    }
+
+
     public void TargetPlacement() 
     {
         target.position = path.GetPointInPath(targetPositions.GetRandomTargetPosition());
+    }
+
+    public void TargetDemo() 
+    {
+        target.position = path.GetPointInPath(0.8f);
     }
 }
