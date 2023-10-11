15db109ea58c85d94d897802196aaa9520c0a2b4
MantasMantas <xmantissx@gmail.com>
2023-10-11 13:33:12 +0200

    target sequence done

--- Assets/ThresholdsExperiment/Scripts/CollisionDetection.cs
+++ Assets/ThresholdsExperiment/Scripts/CollisionDetection.cs
@@ -6,18 +6,26 @@ public class CollisionDetection : MonoBehaviour
 {
 
     public MaterialsList materials;
-    public VoidEvent TargetEvent, StartAreaEvent;
+    public VoidEvent EventToCall;
     public TExperimentConfiguration experimentConfig;
 
     private Renderer renderer;
     private bool counting;
     private float counter, threshold;
-    private VoidEvent currentEvent;
 
     private void OnEnable()
     {
         renderer = GetComponent<Renderer>();
         AssignMaterial(materials.GetDefault());
+
+        if(this.name == "StartArea") 
+        {
+            threshold = experimentConfig.startTouchingDuration;
+        }
+        if(this.name == "Target") 
+        {
+            threshold = experimentConfig.targetTouchDuration;
+        }
     }
 
     private void Update()
@@ -27,7 +35,7 @@ public class CollisionDetection : MonoBehaviour
             if(counter >= threshold) 
             {
                 counting = false;
-                currentEvent.raiseEvent();
+                EventToCall.raiseEvent();
                 counter = 0;
                 return;
             }
@@ -41,29 +49,20 @@ public class CollisionDetection : MonoBehaviour
         if (!counting) 
         {
             counting = true;
-            if (this.name == "StartArea")
-            {
-                threshold = experimentConfig.startTouchingDuration;
-                currentEvent = StartAreaEvent;
-            }
-
-            if (this.name == "Target")
-            {
-                threshold = experimentConfig.targetTouchDuration;
-                currentEvent = TargetEvent;
-            }
 
         }
        
-
         AssignMaterial(materials.GetTouching());
     }
 
     private void OnTriggerExit(Collider other)
     {
-        counting = false;
-        counter = 0;
-
+        if (counting) 
+        {
+            counting = false;
+            counter = 0;
+        }
+        
         AssignMaterial(materials.GetDefault());
     }
 
--- Assets/ThresholdsExperiment/Scripts/TExperimentManager.cs
+++ Assets/ThresholdsExperiment/Scripts/TExperimentManager.cs
@@ -25,5 +25,10 @@ public class TExperimentManager : MonoBehaviour
         
     }
 
+    public void TargetEvent() 
+    {
+        TimerStart.raiseEvent(experimentConfig.restDuration);
+    }
+
     
 }
