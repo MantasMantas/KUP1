b7512d7f0d469ad6051df9dd3084d0d729c10418
MantasMantas <xmantissx@gmail.com>
2023-10-20 10:32:45 +0200

    hand vibration mechanism is complete

--- Assets/ThresholdsExperiment/Scripts/HandManager.cs
+++ Assets/ThresholdsExperiment/Scripts/HandManager.cs
@@ -7,90 +7,55 @@ using UnityEngine;
 public class HandManager : MonoBehaviour
 {
     public OVRHand rightHand;
-    public float movementThreshold = 0.05f;
+    public float distanceThreshold;
+    public TExperimentConfiguration experimentalConfig;
+    public VoidEvent HandMovementEvent;
 
-    public VoidEvent HandMovementEvent, HandStopEvent;
+    private Vector3 StartingHandPosition, CurrentHandPosition;
+    private bool isVibrationStarted, isVibrationEnabled;
 
-    private Vector3 CurrentHandPosition, PreviousHandPosition;
-    private bool isMoving, isVibrationEnabled;
-
-    // filtering stuff
-    private Queue<Vector3> positions = new Queue<Vector3>();
-    public int bufferSize = 5;
-    private Vector3 smoothedPosition;
 
     // Start is called before the first frame update
     void Start()
     {
-        isVibrationEnabled = false;
-        isMoving = false;
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!isVibrationEnabled)
+        if (!isVibrationEnabled || isVibrationStarted)
+
         {
             return;
         }
 
-        UpdatePosition(rightHand.transform.position);
-        CheckMovement();
+
+        if(Vector3.Distance(StartingHandPosition, CurrentHandPosition) >= distanceThreshold) 
+        {
+            isVibrationStarted = true;
+            HandMovementEvent.raiseEvent();
+        }
+        else 
+        {
+            CurrentHandPosition = rightHand.transform.position;
+        }
         
 
     }
 
 
-    private void CheckMovement() 
-    {
-        if (Vector3.Distance(smoothedPosition, PreviousHandPosition) > movementThreshold)
-        {
-
-            if (!isMoving)
-            {
-                isMoving = true;
-                HandMovementEvent.raiseEvent();
-            }
-
-
-            PreviousHandPosition = smoothedPosition;
-        }
-        else
-        {
-            if (isMoving)
-            {
-                isMoving = false;
-                HandStopEvent.raiseEvent();
-
-            }
-        }
-    }
-    private void UpdatePosition(Vector3 newPosition) 
-    {
-        if (positions.Count >= bufferSize)
-        {
-            positions.Dequeue();
-        }
-
-        positions.Enqueue(newPosition);
-
-        smoothedPosition = Vector3.zero;
-        foreach(var position in positions) 
-        {
-            smoothedPosition += position;
-        }
-
-        smoothedPosition /= positions.Count;
-    }
-
     public void EnableVibration() 
     {
         isVibrationEnabled = true;
+        StartingHandPosition = rightHand.transform.position;
+        CurrentHandPosition = rightHand.transform.position;
     }
 
     public void DisableVibration() 
     {
         isVibrationEnabled = false;
+        isVibrationStarted = false;
     }
 
    
