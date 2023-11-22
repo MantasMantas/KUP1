12ab6a934e4a8fc4c43bf364d0590c6b7537441e
MantasMantas <xmantissx@gmail.com>
2023-11-22 15:03:47 +0100

    new gain thing started

--- Assets/ThresholdsExperiment/Scripts/Managers/HandManager.cs
+++ Assets/ThresholdsExperiment/Scripts/Managers/HandManager.cs
@@ -20,11 +20,14 @@ public class HandManager : MonoBehaviour
     private bool isVibrationStarted, isVibrationEnabled;
     private bool pinchDetect, gainEnabled;
 
+    // Movement delta stuff
+    private Queue<float> positionBuffer = new Queue<float>();
+    private int bufferSize = 5;
 
     // Start is called before the first frame update
     void Start()
     {
-       
+        InitializeBufer();
     }
 
     // Update is called once per frame
@@ -108,17 +111,34 @@ public class HandManager : MonoBehaviour
     private void GainUpdate() 
     {
         // Checking if there is a gain to apply
-        float CurrentGain = experimentalConfig.GetCurrentGain();
+        float currentGain = experimentalConfig.GetCurrentGain();
 
-        if(CurrentGain == 1 || !gainEnabled) 
+        if(currentGain == 1 || !gainEnabled) 
         {
             return;
         }
 
         // Fetching all the necessary variables for calculation
-        float targetPoint = experimentalConfig.GetCurrentTarget();
+        // float targetPoint = experimentalConfig.GetCurrentTarget();
         float currentPointOnPath = path.GetClosestCurveValue(righHandPos.position);
-        float normalizedDistance = 0f;
+
+        if (positionBuffer.Count >= bufferSize)
+        {
+            positionBuffer.Dequeue();
+        }
+
+        positionBuffer.Enqueue(currentPointOnPath);
+
+        float currentDelta = CalculateAverageMovementDelta();
+
+        float deltaGain = currentDelta * currentGain;
+
+        rightHand.transform.position = path.GetPointInPath(deltaGain);
+
+        Debug.Log("Current point on path: " + currentPointOnPath + "Point on path with gain")
+
+
+        /*float normalizedDistance = 0f;
 
         // calculating the normalized distance used for linear interpolation
         if(targetPoint > 0.5f) 
@@ -142,10 +162,32 @@ public class HandManager : MonoBehaviour
         //rightHand.transform.position = new Vector3(rightHand.transform.position.x, curveReference.y, curveReference.z);
         rightHand.transform.position = new Vector3(curveReference.x, rightHand.transform.position.y, curveReference.z);
         //rightHand.transform.position = new Vector3(curveReference.x, curveReference.y, rightHand.transform.position.z);
-        //rightHand.transform.rotation = path.GetRotationInPath(gainPointOnPath);
+        //rightHand.transform.rotation = path.GetRotationInPath(gainPointOnPath);*/
 
     }
 
+    private void InitializeBufer() 
+    {
+        for (int i = 0; i < bufferSize; i++)
+        {
+            positionBuffer.Enqueue(path.GetClosestCurveValue(righHandPos.position));
+        }
+    }
 
-   
+    private float CalculateAverageMovementDelta() 
+    {
+        float SumOfDeltas = 0f;
+        float previousPosition = positionBuffer.Peek();
+
+        foreach(var position in positionBuffer) 
+        {
+            float delta = position - previousPosition;
+            SumOfDeltas += delta;
+            previousPosition = position;
+        }
+
+        return SumOfDeltas / (positionBuffer.Count - 1);
+    }
+
+
 }
